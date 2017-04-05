cc0d80f6a30dc60ecca6aa26d85f247c5d4f04a6
yongqianvip <yongqainvip@163.com>
2017-04-05 17:22:03 +0800

    add source file && README.md

---
+++ NavigationBar.js
@@ -0,0 +1,290 @@
+
+import React from 'react';
+import {
+	View,
+	Text,
+	Image,
+	Platform,
+	StyleSheet,
+	TouchableOpacity,
+	Alert
+} from 'react-native';
+
+import backIcon from '../../assets/img/back.png';
+
+export default class NavigatorBar extends React.Component {
+	constructor(props) {
+	  super(props);
+
+	  this.state = {};
+	  this._goback = this._goback.bind(this);
+	}
+
+	_goback() {
+		if (this.props.backIconClick) {
+			this.props.backIconClick();
+		} else {
+			if (this.props.navigator && !this.props.hiddenBackIcon) this.props.navigator.pop();
+		}
+	}
+	static defaultProps = {
+		leftButtonConfig: {
+			type: 'image',
+			image: backIcon,
+			onClick: ()=>{
+				Alert.alert('default backIcon on click','222222',[{text:'ok',onPress:()=>console.log("124356")}]);
+			}
+		}
+	}
+
+	render() {
+		const { navigator,
+			title,
+			centerIcon,
+			centerIconStyle,
+
+			leftButtonHidden,
+
+			leftButtonConfig,
+			leftIconFont,
+
+			rightButtonConfig,
+			rightIconFont,
+			rightSubButtonConfig,
+			rightSubIconFont,
+
+			style,
+
+		} = this.props;
+
+		return (
+			<View style={ [styles.container, style] }>
+				<View style={ styles.contentContainer }>
+					<View style={ styles.leftContainer }>
+						{
+							(() => {
+								if(!leftButtonHidden) {
+									if (leftButtonConfig) {
+										if (leftButtonConfig.type === 'image') {
+											// 左侧图片按钮
+											return (
+												<TouchableOpacity
+													activeOpacity={ leftButtonConfig.disable ? 1 : (leftButtonConfig.activeOpacity || 0.7) }
+													onPress={ leftButtonConfig.disable ? ()=>{console.log("--leftButton is disabled");} : leftButtonConfig.onClick }>
+													<View>
+														<Image style={ [styles.leftImageStyle,leftButtonConfig.imageStyle] } source={ leftButtonConfig.disable ? leftButtonConfig.disableImage : leftButtonConfig.image }/>
+													</View>
+												</TouchableOpacity>
+											)
+										}else if (leftButtonConfig.type === 'string' || leftButtonConfig.type === 'font') {
+											// 左侧纯文本按钮
+											return (
+												<TouchableOpacity
+													activeOpacity={ leftButtonConfig.disable ? 1 : (leftButtonConfig.activeOpacity || 0.7) }
+													onPress={ leftButtonConfig.disable ? ()=>{console.log("--leftButton is disabled");} : leftButtonConfig.onClick }>
+													<Text style={ [(leftButtonConfig.type === 'string' ? styles.leftTitleStyle : styles.leftIconFontStyle), leftButtonConfig.titleStyle,leftButtonConfig.disable ? {color: leftButtonConfig.disableColor || 'gray'} : {}]}>{ leftButtonConfig.type === 'string' ? leftButtonConfig.title : leftIconFont }</Text>
+												</TouchableOpacity>
+											)
+										}else{
+											return null
+										}
+									}
+								}else{
+									// 隐藏 左按钮
+									return null
+								}
+							})()
+						}
+					</View>
+					<View style={ styles.centerContainer }>
+						{
+							title ?
+								<Text style={ styles.centerTextStyle }>{ title }</Text>
+							:
+								<Image resizeMode='stretch' style={ [styles.centerImg, centerIconStyle] } source={ centerIcon }/>
+						}
+					</View>
+
+					<View style={ styles.rightContainer }>
+						{
+							(() => {
+								if (rightButtonConfig) {
+									if (rightSubButtonConfig) {
+										// 多个按钮
+										if (rightButtonConfig.type === 'image' && rightSubButtonConfig.type === 'image') {
+											return (
+												<View style={{flexDirection: 'row',alignItems:'center',justifyContent:'center'}}>
+													<TouchableOpacity
+														activeOpacity={ rightButtonConfig.disable ? 1 : (rightButtonConfig.activeOpacity || 0.7) }
+														onPress={ rightButtonConfig.disable ? ()=>{console.log("---rightButton is disabled");} : rightButtonConfig.onClick }>
+														<View>
+															<Image style={ [styles.rightImageStyle,rightButtonConfig.imageStyle] } source={ rightButtonConfig.disable ? rightButtonConfig.disableImage : rightButtonConfig.image }/>
+														</View>
+													</TouchableOpacity>
+													<TouchableOpacity
+														activeOpacity={ rightSubButtonConfig.disable ? 1 : (rightSubButtonConfig.activeOpacity || 0.7) }
+														onPress={ rightSubButtonConfig.disable ? ()=>{console.log("---rightSubButton is disabled");} : rightSubButtonConfig.onClick }>
+														<View>
+															<Image style={ [styles.rightImageStyle,rightSubButtonConfig.imageStyle] } source={ rightSubButtonConfig.disable ? rightSubButtonConfig.disableImage : rightSubButtonConfig.image }/>
+														</View>
+													</TouchableOpacity>
+												</View>
+											)
+										}else if (rightButtonConfig.type != 'image' && rightSubButtonConfig.type != 'image') {
+											return (
+												<View style={{flexDirection: 'row',alignItems:'center',justifyContent:'center'}}>
+													<TouchableOpacity
+														activeOpacity={ rightButtonConfig.disable ? 1 : (rightButtonConfig.activeOpacity || 0.7) }
+														onPress={ rightButtonConfig.disable ? ()=>{console.log("--rightButton is disabled")} : rightButtonConfig.onClick }>
+														<Text style={ [(rightButtonConfig.type === 'string' ? styles.rightTitleStyle : styles.rightIconFontStyle), rightButtonConfig.titleStyle,rightButtonConfig.disable ? {color: rightButtonConfig.disableColor || 'gray'} : {}] }>{ rightButtonConfig.type === 'string' ? rightButtonConfig.title : rightIconFont }</Text>
+													</TouchableOpacity>
+													<TouchableOpacity
+														activeOpacity={ rightSubButtonConfig.disable ? 1 : (rightSubButtonConfig.activeOpacity || 0.7) }
+														onPress={ rightSubButtonConfig.disable ? ()=>{console.log("--rightSubButton is disabled");} : rightSubButtonConfig.onClick }>
+														<Text style={ [(rightSubButtonConfig.type === 'string' ? styles.rightTitleStyle : styles.rightIconFontStyle), rightSubButtonConfig.titleStyle,rightSubButtonConfig.disable ? {color: rightSubButtonConfig.disableColor || 'gray'} : {}]}>{ rightSubButtonConfig.type === 'string' ? rightSubButtonConfig.title : rightSubIconFont }</Text>
+													</TouchableOpacity>
+												</View>
+											)
+										}else{
+											console.warn("right button 不支持两个不同类型的button，例如rightButton.type='image',rightSubButton.type='string'");
+											return null
+										}
+									}else{
+										// 单个按钮
+										if (rightButtonConfig.type === 'image') {
+											// 右侧图片按钮
+											return (
+												<TouchableOpacity
+													activeOpacity={ rightButtonConfig.disable ? 1 : (rightButtonConfig.activeOpacity || 0.7) }
+													onPress={ rightButtonConfig.disable ? ()=>{console.log("--rightButton is disabled");} : rightButtonConfig.onClick }>
+													<View>
+														<Image style={ [styles.rightImageStyle,rightButtonConfig.imageStyle] } source={ rightButtonConfig.disable ? rightButtonConfig.disableImage : rightButtonConfig.image }/>
+													</View>
+												</TouchableOpacity>
+											)
+										}else if (rightButtonConfig.type === 'font' || rightButtonConfig.type === 'string') {
+											// 右侧纯文本按钮
+											return (
+												<TouchableOpacity
+													activeOpacity={ rightButtonConfig.disable ? 1 : (rightButtonConfig.activeOpacity || 0.7) }
+													onPress={ rightButtonConfig.disable ? ()=>{console.log("--rightButton is disabled");} : rightButtonConfig.onClick }>
+													<Text style={ [(rightButtonConfig.type === 'string' ? styles.rightTitleStyle : styles.rightIconFontStyle), rightButtonConfig.titleStyle,rightButtonConfig.disable ? {color: rightButtonConfig.disableColor || 'gray'} : {}] }>{ rightButtonConfig.type === 'string' ? rightButtonConfig.title : rightIconFont }</Text>
+												</TouchableOpacity>
+											)
+										}
+									}
+								}else{
+									// 没有按钮
+									return null
+								}
+							})()
+						}
+					</View>
+
+				</View>
+			</View>
+		);
+	}
+}
+
+const propTypes = {
+  centerIconStyle: Text.propTypes.style,
+
+  title: React.PropTypes.string,
+  hiddenBackIcon: React.PropTypes.bool,
+  rightClick: React.PropTypes.func,
+  backIconClick: React.PropTypes.func,
+}
+
+NavigatorBar.propTypes = propTypes;
+
+const styles = StyleSheet.create({
+	container: {
+		...Platform.select({
+			ios: {
+				height: 64,
+			},
+			android: {
+				height: 50
+			}
+		}),
+		backgroundColor: '#008dcf',
+	},
+	contentContainer: {
+		flex: 1,
+		...Platform.select({
+			ios: {
+				paddingTop: 15,
+			},
+			android: {
+				paddingTop: 0,
+			}
+		}),
+		flexDirection: 'row',
+	},
+	leftContainer: {
+		flex: 1,
+		justifyContent: 'center',
+
+	},
+	centerContainer: {
+		flex: 2,
+		justifyContent: 'center',
+	},
+	rightContainer: {
+		flex: 1,
+		alignItems: 'flex-end',
+		justifyContent: 'center',
+
+	},
+	leftImageStyle: {
+		width: 20,
+		height: 20,
+		marginLeft: 10,
+	},
+	rightImageStyle: {
+		width: 20,
+		height: 20,
+		marginRight: 10,
+	},
+	centerTextStyle: {
+		textAlign: 'center',
+		color: 'white',
+		fontSize: 18,
+	},
+	leftIconFontStyle: {
+		marginLeft: 10,
+		fontSize: 20,
+		color: 'white',
+		fontFamily: 'iconfont',
+	},
+	rightIconFontStyle: {
+		marginRight: 10,
+		fontSize: 20,
+		color: 'white',
+		fontFamily: 'iconfont',
+	},
+	leftTitleStyle: {
+		color: 'white',
+		marginLeft: 10,
+	},
+	rightTitleStyle: {
+		color: 'white',
+		marginRight: 10
+	},
+	centerImg: {
+		alignSelf: 'center',
+	},
+	unreading: {
+		backgroundColor: 'red',
+		borderRadius:6,
+		width:12,
+		height: 12,
+		top: 10,
+		right: 10,
+		position: 'absolute'
+	},
+	iconFont: {
+		fontFamily: 'iconfont',
+	},
+});
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # RN_NavigationBar
 common navigationBar for react-native
+
+
+##  NavigationBar
+### 三种类型
+
+- image
+
+		leftButtonConfig={{
+			type: 'image',
+			image: signForImg,
+			imageStyle:{width: 20,height:20},
+			onClick: ()=>{
+	
+			}
+		}}
+		
+- font
+
+		leftIconFont='&#xe617;'
+		leftButtonConfig={{
+			type: 'font',
+			disable: true,
+			disableColor: 'red',
+			onClick: ()=>{
+				
+			}
+		}}
+	
+- string
+
+		leftButtonConfig={{
+ 			type: 'string',
+	 		disable: false,
+	 		title: '编辑订单',
+	 		onClick: ()=>{
+	 			
+	 		}
+	 	}}
+		 	
+- rightButton && rightSubButton
+	
+		rightIconFont= '&#xe617;'
+		rightButtonConfig={{
+			type: 'font',
+			titleStyle:{fontSize:25},
+			disable: true,
+			disableColor: 'red',
+			onClick: ()=>{
+				Alert.alert('right font 按钮','text button clicked',[{text: 'ok',onPress:()=>{console.log("----image button cilcked")}}])
+			}
+		}}
+		rightSubIconFont= '&#xe617;'
+		rightSubButtonConfig={{
+			type: 'font',
+			onClick: ()=>{
+				Alert.alert('right sub 按钮','text button clicked',[{text: 'ok',onPress:()=>{console.log("----image button cilcked")}}])
+			}
+		}}
